1f38e25273a6bfecfa7ce391b4ee6979e0fd37a1
Christian Egli <christian.egli@sbs.ch>
2015-10-13 14:47:55 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject catalog "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/sbsdev/catalog"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main ^:skip-aot catalog.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
