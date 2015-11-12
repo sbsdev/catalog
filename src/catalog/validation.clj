3828f496cbf4b3f331e85c2335c9b854f08d8164
Christian Egli <christian.egli@sbs.ch>
2015-11-12 15:19:22 +0100

    Refine the schema to be mostly accurate

--- src/catalog/validation.clj
+++ src/catalog/validation.clj
@@ -3,6 +3,29 @@
             [schema.core :as s]
             [schema.experimental.abstract-map :as abstract-map]))
 
+(def Language
+  (apply s/enum (conj (vals vubis/iso-639-2-to-iso-639-1) "und")))
+
+(def Genre
+  (apply s/enum (vals vubis/genre-raw-to-genre)))
+
+(def SubGenre
+  (apply s/enum (vals vubis/genre-raw-to-subgenre)))
+
+(def ProducerBrief
+  (apply s/enum (set (vals vubis/producer-raw-to-producer))))
+
+(def BrailleGrade
+  (apply s/enum (vals vubis/braille-grade-raw-to-braille-grade)))
+
+(def SignatureTuple
+  [(s/one s/Str "signature")
+   (s/one BrailleGrade "grade")
+   (s/one s/Str "volumes")])
+
+(def LibrarySignature
+  (s/if map? {BrailleGrade [SignatureTuple]} s/Str))
+
 (s/defschema CatalogItem
   (abstract-map/abstract-map-schema
    :format
@@ -11,58 +34,81 @@
     :title s/Str
     (s/optional-key :subtitle) s/Str
     (s/optional-key :name-of-part) s/Str
-    :source s/Str
-    :source-publisher s/Str
-    :source-date s/Inst
-    :genre (apply s/enum (vals vubis/genre-raw-to-genre))
-    :sub-genre (apply s/enum (vals vubis/genre-raw-to-subgenre))
+    (s/optional-key :source) s/Str
     (s/optional-key :description) s/Str
-    :producer-brief (apply s/enum (set (vals vubis/producer-raw-to-producer)))
-    :library-signature s/Str
+    :library-signature LibrarySignature
     (s/optional-key :product-number) s/Str
     (s/optional-key :price) s/Str
     (s/optional-key :accompanying_material) s/Str
-    :language (apply s/enum (conj (vals vubis/iso-639-2-to-iso-639-1) "und"))
+    (s/optional-key :language) Language
     }))
 
 (abstract-map/extend-schema
  Hörbuch CatalogItem [:hörbuch]
- {:duration s/Int
+ {:source-publisher s/Str
+  :source-date s/Inst
+  :genre Genre
+  :sub-genre SubGenre
+  :producer-brief ProducerBrief
+  :duration s/Int
   :narrators [s/Str]
   :produced-commercially? s/Bool})
 
 (abstract-map/extend-schema
  Braille CatalogItem [:braille]
- {:rucksackbuch? s/Bool
-  (s/optional-key :rucksackbuch-number) s/Int
-  :braille-grade (apply s/enum (vals vubis/braille-grade-raw-to-braille-grade))
-  :volumes s/Str})
+ {:source-publisher s/Str
+  :source-date s/Inst
+  :genre Genre
+  :producer-brief ProducerBrief
+  :sub-genre SubGenre
+  :rucksackbuch? s/Bool
+  (s/optional-key :rucksackbuch-number) s/Int})
 
 (abstract-map/extend-schema
  Taktil CatalogItem [:taktilesbuch]
- {})
+ {:source-publisher s/Str
+  :source-date s/Inst})
 
 (abstract-map/extend-schema
  Musiknoten CatalogItem [:musiknoten]
- {})
+ {:source-publisher s/Str
+  :source-date s/Inst
+  :producer-brief ProducerBrief})
 
 (abstract-map/extend-schema
  Grossdruck CatalogItem [:grossdruck]
- {:volumes s/Str})
+ {:source-publisher s/Str
+  :source-date s/Inst
+  :genre Genre
+  :sub-genre SubGenre
+  :producer-brief ProducerBrief
+  :volumes s/Str})
 
 (abstract-map/extend-schema
  E-book CatalogItem [:e-book]
- {})
+ {:source-publisher s/Str
+  :source-date s/Inst
+  :producer-brief ProducerBrief
+  :genre Genre
+  :sub-genre SubGenre})
 
 (abstract-map/extend-schema
  Hörfilm CatalogItem [:hörfilm]
- {:producer s/Str
+ {:genre (apply s/enum (vals vubis/genre-code-to-genre))
+  :producer s/Str
   :personel-name s/Str
   :movie_country s/Str})
 
 (abstract-map/extend-schema
  Spiel CatalogItem [:ludo]
- {:game-category s/Str
+ {:source-publisher s/Str
+  :source-date s/Inst
+  :game-category s/Str
   :game-description s/Str
   :game-materials s/Str})
 
+(defn distinct-titles? [items]
+  (and (> (count items) 1)
+       (->> items
+            (map :title)
+            (apply distinct?))))
