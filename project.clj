2156e57edd86173d1c7dc3edbb1d39d266f6a8aa
Christian Egli <christian.egli@sbs.ch>
2015-11-23 15:51:01 +0100

    We need data.xml for dtbook generation

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [medley "0.7.0"]
                  [prismatic/schema "1.0.3"]
                  [clj-time "0.11.0"]
-                 [comb "0.1.0" :exclusions [org.clojure/clojure]]]
+                 [comb "0.1.0" :exclusions [org.clojure/clojure]]
+                 [org.clojure/data.xml "0.0.8"]]
   :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
             [refactor-nrepl "2.0.0-SNAPSHOT"]]
   :main ^:skip-aot catalog.core
