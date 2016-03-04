83632f1d7881b4336e0009db38b71276cd386153
Christian Egli <christian.egli@sbs.ch>
2016-03-04 17:07:43 +0100

    Add an event listener to fop

--- src/catalog/layout/fop.clj
+++ src/catalog/layout/fop.clj
@@ -12,7 +12,8 @@
            javax.xml.transform.sax.SAXResult
            javax.xml.transform.stream.StreamSource
            javax.xml.transform.TransformerFactory
-           [org.apache.fop.apps FopFactory MimeConstants]))
+           [org.apache.fop.apps FopFactory MimeConstants]
+           [org.apache.fop.events EventListener EventFormatter]))
 
 (def ^:private region-body-margin-top 15)
 
@@ -683,8 +684,17 @@
   (with-open [out (io/output-stream out)]
     (let [;; Construct a FopFactory by specifying a reference to the configuration file
           factory (FopFactory/newInstance (io/file (io/resource "fop.xconf")))
+          ;; get a user agent and add a listener to it that logs events
+          user-agent (.newFOUserAgent factory)
+          broadcaster (.getEventBroadcaster user-agent)
+          _ (.addEventListener broadcaster
+                               (reify EventListener
+                                 (processEvent [this event]
+                                   (let [id (.getEventID event)]
+                                     (when (not= id "org.apache.fop.render.RendererEventProducer.endPage")
+                                       (println (EventFormatter/format event)))))))
           ;; Construct fop with desired output format
-          fop (.newFop factory MimeConstants/MIME_PDF out)
+          fop (.newFop factory MimeConstants/MIME_PDF user-agent out)
           ;; Setup JAXP using identity transformer
           transformer (.newTransformer (TransformerFactory/newInstance))
           ;; Setup input and output for XSLT transformation
