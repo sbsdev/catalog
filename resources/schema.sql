e63a863cc769569b0c98427380f4e8fb074f7b74
Christian Egli <christian.egli@sbs.ch>
2016-04-25 11:21:44 +0200

    Simplify the database design
    Use natural keys instead of artificial keys. The make more sense in this
    context.

---
+++ resources/schema.sql
@@ -0,0 +1,49 @@
+-- Type of a catalog
+-- a classic reference table
+CREATE TABLE catalog_type (
+  id VARCHAR(16) PRIMARY KEY
+);
+
+CREATE TABLE catalogs (
+  year YEAR NOT NULL,
+  issue TINYINT NOT NULL,
+  items MEDIUMTEXT NOT NULL,
+  PRIMARY KEY(year, issue)
+);
+
+CREATE TABLE full_catalogs (
+  year YEAR NOT NULL,
+  catalog_type VARCHAR(16) NOT NULL,
+  items MEDIUMTEXT NOT NULL,
+  PRIMARY KEY(year, catalog_type),
+  FOREIGN KEY(catalog_type) REFERENCES catalog_type(id)
+);
+
+CREATE TABLE recommendations (
+  year YEAR NOT NULL,
+  issue TINYINT NOT NULL,
+  catalog_type VARCHAR(16) NOT NULL,
+  content TEXT NOT NULL,
+  PRIMARY KEY(year, issue, catalog_type),
+  FOREIGN KEY(year, issue) REFERENCES catalogs(year, issue),
+  FOREIGN KEY(catalog_type) REFERENCES catalog_type(id)
+);
+
+CREATE TABLE editorials (
+  year YEAR NOT NULL,
+  issue TINYINT NOT NULL,
+  catalog_type VARCHAR(16) NOT NULL,
+  content TEXT NOT NULL,
+  PRIMARY KEY(year, issue, catalog_type),
+  FOREIGN KEY(year, issue) REFERENCES catalogs(year, issue),
+  FOREIGN KEY(catalog_type) REFERENCES catalog_type(id)
+);
+
+INSERT INTO catalog_type VALUES
+('sortiment'),
+('grossdruck'),
+('braille'),
+('hörbuch'),
+('hörfilm'),
+('ludo'),
+('taktil');
