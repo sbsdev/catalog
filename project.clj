c25cbc9b715d71f0bc4cbf4980221d5fa0507abd
Christian Egli <christian.egli@sbs.ch>
2016-05-04 17:30:03 +0200

    Upgrade yesql

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/sbsdev/catalog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.zip "0.1.1"]
                  [medley "0.7.4"]
                  [prismatic/schema "1.1.1"]
@@ -21,8 +21,9 @@
                  [endophile "0.1.2"]
                  [org.clojure/java.jdbc "0.5.8"]
                  [mysql/mysql-connector-java "6.0.2"]
-                 [yesql "0.5.2"]
-  :plugins [[cider/cider-nrepl "0.12.0-SNAPSHOT"]
+                 [yesql "0.5.3"]
+                 [bidi "2.0.8"]]
+  :plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]
             [refactor-nrepl "2.3.0-SNAPSHOT"]
             [org.clojars.cvillecsteele/lein-git-version "1.0.3"]]
   :main ^:skip-aot catalog.core
