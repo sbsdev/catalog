73baecf4d38713e8fcc757a98fd035deca37f794
Christian Egli <christian.egli@sbs.ch>
2016-09-07 12:31:39 +0200

    Use the datasource to connect to the db
    instead of specifying the connection directly

--- src/catalog/db.clj
+++ src/catalog/db.clj
@@ -2,9 +2,9 @@
   (:require [clojure.edn :as edn]
             [yesql.core :refer [defqueries]]))
 
-#_(def ^:private db {:factory factory :name "java:jboss/datasources/catalog"})
+(def ^:private db {:name "java:jboss/datasources/catalog"})
 
-(def ^:private db "jdbc:mysql://localhost:3306/catalog?user=catalog&serverTimezone=UTC")
+#_(def ^:private db "jdbc:mysql://localhost:3306/catalog?user=catalog&serverTimezone=UTC")
 
 (defqueries "catalog/queries.sql" {:connection db})
 
