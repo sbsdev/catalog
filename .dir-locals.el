93fa1c9310b9d1038bccfe73dbf3e99b0d24079a
Christian Egli <christian.egli@sbs.ch>
2016-11-08 15:23:59 +0100

    Define compilation command for emacs

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (compile-command . "LEIN_SNAPSHOTS_IN_RELEASE=yes lein immutant war --nrepl-start --nrepl-port 40021")))
