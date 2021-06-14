eafc5b81dfb47a4a6652f9fc2be162bf111da440
Christian Egli <christian.egli@sbs.ch>
2021-06-14 15:03:21 +0200

    Version 0.28.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject catalog "0.27.0"
+(defproject catalog "0.28.0-SNAPSHOT"
   :description "A webapp to generate catalogs of the new items in the library"
   :url "https://github.com/sbsdev/catalog"
   :license {:name "GNU Affero General Public License"
